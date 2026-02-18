a1e08c24655cefeb3e3ff679ae8e2d2352f7fe30
Emmanuelsuotor <suotoremmanuelhenry@gmail.com>
2026-02-18 11:14:58 +0000

    UserInput

---
+++ JavaOutput.java
@@ -0,0 +1,16 @@
+public class JavaOutput{
+	
+public static void main(String[] args){
+	//Using the println Method
+	System.out.println("Hello world,this is my first Java program");
+	System.out.println("Learning Java Is Fun");
+	
+	
+	//Using the print method
+	System.out.print("Do you Love Learning Java? ");
+	System.out.print("Yes I Love Learning Java ");
+	
+	//Using the printf method
+	System.out.printf("My name is Emmanuel Henry, I am %d Years old %n",25);
+	}
+}
